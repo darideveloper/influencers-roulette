a4319901f58cf6577a8b50c7a179f136b50a5315
Silvia <silviaislam.eera@gmail.com>
2025-10-08 20:26:50 +0600

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()],
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
