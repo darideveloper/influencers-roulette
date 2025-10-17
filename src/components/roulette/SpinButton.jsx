7e2dda2b280ea98d0fa076eec84f7bafd9b20fee
daridev <darideveloper@gmail.com>
2025-10-17 13:00:27 -0600

    feat (SpinButton): manage styles with talwind

--- src/components/roulette/SpinButton.jsx
+++ src/components/roulette/SpinButton.jsx
@@ -1,14 +1,19 @@
 export default function SpinButton({ onSpin, disable, text }) {
   return (
-    <div className="text-center">
-      <button 
+    <div className='text-center'>
+      <button
         onClick={onSpin}
         disabled={disable}
-        className="bg-gradient-to-r from-pink-500 to-pink-600 text-white font-black py-2 px-12 text-xl sm:text-xl rounded-full hover:not([disabled]):from-pink-600 hover:not([disabled]):to-pink-700 transition transform hover:not([disabled]):scale-105 active:scale-95 disabled:opacity-70 disabled:cursor-not-allowed disabled:transform-none shadow-2xl uppercase tracking-wider hover:not([disabled]):!cursor-pointer mb-6"
+        className='bg-gradient-to-r from-pink-500 to-pink-600 text-white font-black py-2 px-12 text-xl sm:text-xl rounded-full transition transform active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed disabled:transform-none shadow-2xl uppercase tracking-wider mb-6 duration-300'
+        style={{
+          cursor: disable ? 'not-allowed' : 'pointer',
+          opacity: disable ? 0.5 : 1,
+          transform: disable ? 'none' : 'scale(1.1)',
+          transition: 'all 0.2s ease-in-out',
+        }}
       >
         {text}
       </button>
     </div>
-  );
+  )
 }
-
